2b9655cc418c6639968a1ef3c768e6f7abfaa89a
onknows <onno@onknows.com>
2019-10-11 07:50:38 +0200

    myapp upgrade en rollback

--- roles/internal/lcm/library/lcm_info.py
+++ roles/internal/lcm/library/lcm_info.py
@@ -6,6 +6,7 @@ import glob, os, hashlib
 # Determine operation: install, update, upgrade, 
 #   rollback of rollforward
 def operation(data, vsn_file_current, vsn_file):
+  cv = None
   if os.path.isfile(vsn_file_current):
     cv = open(vsn_file_current, 'r').read()
     if cv == data['version']:
@@ -32,22 +33,42 @@ def operation(data, vsn_file_current, vsn_file):
   else:
     operation = 'install'
     descr = 'Install ' + data['version']
-  return operation, descr
+  return operation, descr, cv
+
+def database_version(data,vsn):
+  if vsn is None:
+    vsn = data['version'] 
+  return data['database'] + '_' + vsn.replace('.', '_')
+
+def home_version(data,vsn):
+  return os.path.join(os.path.sep,data['home'],os.path.basename(data['home']) + '-' + vsn)
+
+def home_link(data):
+  return os.path.join(os.path.sep, data['home'], os.path.basename(data['home']))
+
+def version_file_link(data):
+  return os.path.join(os.path.sep, home_link(data),'VERSION')
+
+def version_file(data,vsn):
+  return os.path.join(os.path.sep, home_version(data,vsn),'VERSION')
 
 def lcm_info(data):
   has_changed = False
   role =  data['role']
-  lnk = os.path.join(os.path.sep, data['home'], os.path.basename(data['home']))
-  vsn_file_current = os.path.join(os.path.sep, lnk,'VERSION')
-  vsn_file = os.path.join(os.path.sep, data['home_version'],'VERSION')
-  if os.path.isdir(data['home_version']):
+  vsn_file_current = version_file_link(data)
+  vsn_file = version_file(data,data['version'])   
+  if os.path.isdir(home_version(data,data['version'])):
     open(vsn_file,'w').write(data['version'])
-  op, op_descr = operation(data,vsn_file_current, vsn_file) 
-
+  op, op_descr, cv = operation(data,vsn_file_current, vsn_file) 
   fcts = {role + '_lcm':{"operation": op,
   "operation-description": op_descr,
   "version-file": vsn_file},
-  role + '_home_link': lnk}
+  role + '_home_link': home_link(data)}
+  fcts[role + '_home_version'] = home_version(data,data['version'])
+  fcts[role + '_database_name_version'] = database_version(data,data['version'])
+  if op == 'upgrade':
+    fcts[role + '_database_template'] = database_version(data,cv)
+    fcts[role + '_home_version_current'] = home_version(data,cv)    
   return (has_changed, fcts)
 
 def main():
@@ -56,7 +77,7 @@ def main():
     "home": {"required": True, "type": "str"},
     "version": {"required": True, "type": "str"},
     "versions_sequence": {"required": True, "type": "list"},
-    "home_version": {"required": True, "type": "str"}}
+    "database": {"required": True, "type": "str"}}
 
   module = AnsibleModule(argument_spec=fields)
   has_changed, fcts = lcm_info(module.params)
